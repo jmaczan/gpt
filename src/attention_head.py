7f49a0ce790a030bc888122f17bc2cb4d6ba7505
jmaczan <jedrzejpawel@maczan.pl>
2024-06-11 17:47:46 +0200

    Very basic and likely wrong implementation of single self-attention head

--- src/attention_head.py
+++ src/attention_head.py
@@ -12,19 +12,45 @@ class AttentionHead(nn.Module):
     def __init__(self, embeddings):
         super().__init__()
 
-        batch, sequence_length, embedding_dim = embeddings.shape
+        _, _, embedding_dim = embeddings.shape
 
-        self.W_Q = torch.empty((sequence_length, embedding_dim))
-        self.W_K = torch.empty((sequence_length, embedding_dim))
-        self.W_V = torch.empty((sequence_length, embedding_dim))
+        self.W_Q = torch.empty((embedding_dim, embedding_dim))
+        self.W_K = torch.empty((embedding_dim, embedding_dim))
+        self.W_V = torch.empty((embedding_dim, embedding_dim))
 
         nn.init.xavier_uniform_(self.W_Q)
         nn.init.xavier_uniform_(self.W_K)
         nn.init.xavier_uniform_(self.W_V)
 
     def forward(self, embeddings):
-        batch, sequence_length, embeddings_dim = embeddings.shape
+        _, sequence_length, embeddings_dim = (
+            embeddings.shape
+        )  # it doesn't take into account splitting embedding_dim per attention head (d_head = d_model / h), where h is number of attention heads
 
-        embeddings = PositionalEncoding(
-            sequence_length=sequence_length, embedding_dim=embeddings_dim
-        ).forward(embeddings)
+        embeddings = PositionalEncoding(  # I think it can be pulled out to __init__ and reused across batches (good)
+            # or pulled to gpt.py and reused across attention heads (better)
+            sequence_length=sequence_length,
+            embedding_dim=embeddings_dim,
+        ).forward(
+            embeddings
+        )
+
+        # compute Q, K and V for each token in each of embeddings
+        Q = embeddings @ self.W_Q
+        K = embeddings @ self.W_K
+        V = embeddings @ self.W_V
+
+        attention_scores = Q @ K.transpose(1, 2)
+
+        # build -inf upper triangle mask
+        mask = torch.triu(attention_scores, diagonal=1)
+        mask = mask.masked_fill(mask == 1, float("-inf"))
+
+        masked_scores = attention_scores + mask
+
+        # softmax
+        probabilities = torch.nn.Softmax(masked_scores)
+
+        output = probabilities @ V
+
+        return output
