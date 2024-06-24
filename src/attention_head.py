dd8e435f421b0f3f5eb31b9799b3db54bcb8f2d0
jmaczan <jedrzejpawel@maczan.pl>
2024-06-24 20:13:03 +0200

    Minor device changes

--- src/attention_head.py
+++ src/attention_head.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 
 
 class AttentionHead(nn.Module):
-
     def __init__(self, embedding_dim):
         super().__init__()
 
@@ -18,24 +17,25 @@ class AttentionHead(nn.Module):
     def forward(self, embeddings):
         batch_size, sequence_length, embeddings_dim = embeddings.shape
 
-        # compute Q, K and V for each token in each of embeddings
         Q = embeddings @ self.W_Q
         K = embeddings @ self.W_K
         V = embeddings @ self.W_V
 
         attention_scores = (Q @ K.transpose(-2, -1)) / torch.sqrt(
-            torch.tensor(embeddings_dim, dtype=torch.float32)
+            torch.tensor(embeddings_dim, dtype=torch.float32, device=embeddings.device)
         )
 
-        # build -inf upper triangle mask
         mask = torch.triu(
-            torch.full((sequence_length, sequence_length), float("-inf")), diagonal=1
+            torch.full(
+                (sequence_length, sequence_length),
+                float("-inf"),
+                device=embeddings.device,
+            ),
+            diagonal=1,
         )
-        mask = mask.unsqueeze(0).expand(batch_size, -1, -1).to(embeddings.device)
+        mask = mask.unsqueeze(0).expand(batch_size, -1, -1)
 
         masked_scores = attention_scores + mask
-
-        # softmax
         probabilities = torch.nn.Softmax(dim=-1)(masked_scores)
 
         output = probabilities @ V
