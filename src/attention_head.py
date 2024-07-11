Gradient clipping, detect anomaly, use pad tokens in data loader, shuffle training data, minor text preprocessing, pin_memory, increase number of workers on DataLoader and fix computation of output in attention head

src/attention_head.py
@@ -23,6 +23,7 @@ class AttentionHead(nn.Module):
 
         Q = self.W_Q(embeddings)
         K = self.W_K(embeddings)
+        V = self.W_V(embeddings)
 
         attention_scores = (Q @ K.transpose(-2, -1)) / torch.sqrt(
             torch.tensor(self.head_size, dtype=torch.float32, device=embeddings.device)
@@ -36,8 +37,6 @@ class AttentionHead(nn.Module):
         attention_scores = torch.nn.Softmax(dim=-1)(attention_scores)
         attention_scores = self.dropout(attention_scores)
 
-        value = self.W_V(attention_scores)
-
-        output = attention_scores @ value
+        output = attention_scores @ V
 
         return output

src/data_loader.py
@@ -6,17 +6,22 @@ from gpt import default_context_window, default_batch_size
 
 class TextDataset(Dataset):
     def __init__(self, text, tokenizer, context_window):
+        self.tokenizer = tokenizer
         self.tokens = tokenizer.encode(text)
         self.context_window = context_window
 
     def __len__(self):
-        return len(self.tokens) - self.context_window
+        return max(0, len(self.tokens) - self.context_window)
 
     def __getitem__(self, index):
         x = self.tokens[index : index + self.context_window]
         y = self.tokens[index + 1 : index + self.context_window + 1]
+        
+        if len(x) < self.context_window:
+            x = x + [self.tokenizer.pad_token_id] * (self.context_window - len(x))
+            y = y + [self.tokenizer.pad_token_id] * (self.context_window - len(y))
+        
         return torch.tensor(x), torch.tensor(y)
-
 
 def verify_tokenizer(tokenizer, text="Rick:\nWhat's up?"):
     tokens = tokenizer.encode(text)
@@ -37,14 +42,20 @@ def get_data_loader(
     tokenizer,
     batch_size=default_batch_size,
     data_path="data/dataset.txt",
-    shuffle=False,
+    shuffle=True,
 ):
 
     with open(data_path, "r") as file:
         text = file.read()
 
+    text = " ".join(text.split())
+
+  
+
     dataset = TextDataset(
         text=text, tokenizer=tokenizer, context_window=default_context_window
     )
+    print(f"Total tokens in dataset: {len(dataset.tokens)}")
+    print(f"Number of samples: {len(dataset)}")
 
-    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=True, num_workers=4)

src/run.py
@@ -81,7 +81,7 @@ def inference(
     return tokenizer.decode(output, skip_special_tokens=True)
 
 
-def run(model_path="checkpoints/best_model.pth", prompt="Rick: What's up?"):
+def run(model_path="checkpoints/best_model.pth", prompt=""):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     tokenizer = get_tokenizer()

src/train.py
@@ -14,7 +14,7 @@ from gpt import (
 
 
 default_num_epochs = 50
-default_learning_rate = 0.001
+default_learning_rate = 3e-4 # 0.001 before, but then I took it from Karpathy's videos
 
 checkpoints_directory = "checkpoints"
 os.makedirs(checkpoints_directory, exist_ok=True)
@@ -102,9 +102,11 @@ def train(
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs.view(-1, vocabulary_size), targets.view(-1))
-            loss.backward()
-            optimizer.step()
+            with torch.autograd.detect_anomaly():
+                loss = criterion(outputs.view(-1, vocabulary_size), targets.view(-1))
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                optimizer.step()
             total_loss += loss.item()
 
         average_loss = total_loss / len(data_loader)